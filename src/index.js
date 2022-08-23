6e39bb897bbbea44f03756b2a207f302199c6cee
mdashik7723 <mdashik7723@gmail.com>
2022-08-24 02:35:36 +0600

    --

--- src/index.js
+++ src/index.js
@@ -4,45 +4,21 @@ import Card from "./Cards";
 import './index.css'
 import Sdata from "./Sdata";
 
-
-console.log(Sdata[0].sname);
-
 ReactDom.render(
     <>
-
-
         <h1 className='heading_style'> List of top 5 Netflix Series 2022 </h1>
-        <Card
-            imgsrc={(Sdata[0].imgsrc)}
-            title={(Sdata[0].title)}
-            sname={(Sdata[0].sname)}
-            link={(Sdata[0].links)}
-        />
+        {Sdata.map((val) => {
+            return (
+                <Card
+                    imgsrc={val.imgsrc}
+                    title={val.title}
+                    sname={val.sname}
+                    link={val.links}
+                />
 
-        <Card
-            imgsrc={(Sdata[1].imgsrc)}
-            title={(Sdata[1].title)}
-            sname={(Sdata[1].sname)}
-            link={(Sdata[1].links)}
-        />
-        <Card
-            imgsrc={(Sdata[2].imgsrc)}
-            title={(Sdata[2].title)}
-            sname={(Sdata[2].sname)}
-            link={(Sdata[2].links)}
-        />
-        <Card
-            imgsrc={(Sdata[3].imgsrc)}
-            title={(Sdata[3].title)}
-            sname={(Sdata[3].sname)}
-            link={(Sdata[3].links)}
-        />
-        <Card
-            imgsrc={(Sdata[4].imgsrc)}
-            title={(Sdata[4].title)}
-            sname={(Sdata[4].sname)}
-            link={(Sdata[4].links)}
-        />
+            );
+        })
+        };
     </>,
     document.getElementById("root")
 );
